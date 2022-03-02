1c07ac55ac9c49f470ac19ab427f895c2d48de7d
SERE <47705847+Buleskyssr@users.noreply.github.com>
2022-03-02 19:20:25 +0800

    Add files via upload

---
+++ Diagnostic SVM-RFE code.R
@@ -0,0 +1,36 @@
+require(e1071)
+require(kernlab)
+require(caret)
+
+set.seed(123)
+inputFile="GeneExp.txt"        
+load ('../../data/Datasets/GeneExp.txt')   
+data=read.table(inputFile, header=T, sep="\t", check.names=F, row.names=1)
+data=t(data)
+group=gsub("(.*)\\_(.*)", "\\2", row.names(data))
+
+#SVM-RFE analysis
+Profile=rfe(x=data,
+            y=as.numeric(as.factor(group)),
+            sizes = c(2,4,6,8, seq(10,40,by=3)),
+            rfeControl = rfeControl(functions = caretFuncs, method = "cv"),
+            methods="svmRadial")
+
+# Draw graphics
+pdf(file="SVM-RFE.pdf", width=6, height=5.5)
+par(las=1)
+x = Profile$results$Variables
+y = Profile$results$RMSE
+plot(x, y, xlab="Variables", ylab="RMSE (Cross-Validation)", col="darkgreen")
+lines(x, y, col="darkgreen")
+# Mark the point with the smallest cross validation error
+wmin=which.min(y)
+wmin.x=x[wmin]
+wmin.y=y[wmin]
+points(wmin.x, wmin.y, col="blue", pch=16)
+text(wmin.x, wmin.y, paste0('N=',wmin.x), pos=2, col=2)
+dev.off()
+
+# Output selected genes 
+featureGenes=Profile$optVariables
+write.table(file="SVM-RFE.gene.txt", featureGenes, sep="\t", quote=F, row.names=F, col.names=F)
